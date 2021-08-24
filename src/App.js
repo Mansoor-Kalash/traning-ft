b2c294e66db95a64cdb3c82fcb84d0b9454c3352
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-08-24 16:17:20 +0300

    lab-11

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import React from 'react';
+import BestBooks  from "./BestBooks";
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
+import Profile from './Profile';
 import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import LoginButton from "./LoginButton";
+import { withAuth0 } from '@auth0/auth0-react';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 class App extends React.Component {
 
   render() {
-    console.log('app', this.props);
+    const {isAuthenticated} = this.props.auth0;
     return(
       <>
         <Router>
-          <IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
             <Header />
             <Switch>
               <Route exact path="/">
+              {(!isAuthenticated ?<LoginButton/>:<BestBooks/>)}
+
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/profile">
+                <Profile/>
+              </Route>
             </Switch>
             <Footer />
-          </IsLoadingAndError>
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
   }
 }
 
-export default App;
+export default withAuth0(App);
